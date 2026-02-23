1fe3ef01abb7a531111d172bfad9d5237206df87
LetmarS <letmarsevillano05@gmail.com>
2026-02-23 18:33:49 -0400

    Prueba color fondo de ventana

--- core/src/main/java/com/lavendersalem/game/LavenderSalemGame.java
+++ core/src/main/java/com/lavendersalem/game/LavenderSalemGame.java
@@ -18,7 +18,7 @@ public class LavenderSalemGame extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0, 0, 0, 0);
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
