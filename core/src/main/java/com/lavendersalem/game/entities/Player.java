2e306dee462c1ec22ec27c309a8d92fe9f8028ea
LetmarS <letmarsevillano05@gmail.com>
2026-03-12 22:47:53 -0400

    Separacion de movimiento en Y y en X, agregado de metodo morir para respawn y getter y setters

--- core/src/main/java/com/lavendersalem/game/entities/Player.java
+++ core/src/main/java/com/lavendersalem/game/entities/Player.java
@@ -6,11 +6,14 @@ import com.lavendersalem.game.utils.Constants;
 public abstract class Player extends Entity { // Player hereda de Entity porque es para entidades jugables
     //Atributos de Player
     protected boolean onSuelo; // Si el personaje esta en el suelo
-    protected boolean vivo; // Si esta vivo o muerto (true/false)
+    protected boolean vivo;
     protected int vidas; // Numero de vidas del personaje
     // Constantes físicas de cada persona (Definidas en Constants)
     protected float moviEnX; // Velocidad en carrera
     protected float fuerzaSalto; // Para el salto de cada personaje (contraresta gravedad)
+    // Para optimizar el salto en mov
+    protected float tiempoBufferSalto = 0f;
+    protected static final float MAX_BUFFER_SALTO = 0.1f; // 100ms de ventana
     // Constructor
     public Player(float x, float y, float tamWidth, float tamHeight) {
         super(x, y, tamWidth, tamHeight);
@@ -20,20 +23,24 @@ public abstract class Player extends Entity { // Player hereda de Entity porque 
     // Para actualizar el estado y movimiento del player
     @Override
     public void update(float delta) {
+        if (tiempoBufferSalto > 0) tiempoBufferSalto -= delta; // Reducir buffer cada frame
         configMov();
         aplicGravedad(delta);
-        movimiento(delta);
     }
     protected abstract void configMov(); // Cada player tiene su configuracion de movimientos
     // Para que el player quede sobre el suelo y en caso de salto aplica gravedad
     private void aplicGravedad(float delta) {
         if(!onSuelo){
             velocidad.y += Constants.GRAVEDAD * delta;
+            // Tope de velocidad de caida para evitar traspaso de tiles
+            if (velocidad.y < -960f) velocidad.y = -960f;
         }
     }
-    // Para el movimiento como tal
-    private void movimiento(float delta) {
+    // Para el movimiento como tal (En X y en Y)
+    public void movEnX(float delta) {
         posicion.x += velocidad.x * delta; // La posicion cambia si hay mov (velocidad)
+    }
+    public void movEnY(float delta) {
         posicion.y += velocidad.y * delta;
     }
     // Para "dibujar" el sprite
@@ -43,4 +50,21 @@ public abstract class Player extends Entity { // Player hereda de Entity porque 
             batch.draw(currentFrame, posicion.x, posicion.y, tamWidth, tamHeight);
         }
     }
+    /* PARA CUANDO MUERE O RESPAWNEA */
+    public void morir(float spawnX, float spawnY) {
+        vidas--;
+        posicion.set(spawnX, spawnY);
+        velocidad.set(0, 0);
+        setOnSuelo(false);
+        if (vidas <= 0) vivo = false;
+    }
+    // GETTERS Y SETTER PARA EL GAMESCREEN
+    public boolean isOnSuelo() { return onSuelo; }
+    public void setOnSuelo(boolean onSuelo) { this.onSuelo = onSuelo; }
+
+    public boolean isVivo() { return vivo; }
+    public void setVivo(boolean vivo) { this.vivo = vivo; }
+
+    public int getVidas() { return vidas; }
+    public void setVidas(int vidas) { this.vidas = vidas; }
 }
