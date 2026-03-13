059d8e08b9024b33839e16d46a6aa91057c44b93
LetmarS <letmarsevillano05@gmail.com>
2026-03-12 22:46:19 -0400

    Integracion mecanica de recepcion de vidas de Lavender

--- core/src/main/java/com/lavendersalem/game/entities/Lavender.java
+++ core/src/main/java/com/lavendersalem/game/entities/Lavender.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.lavendersalem.game.utils.Constants;
 
 public class Lavender extends Player{
+    private boolean esperaRescate = false;
     // Constructor (Crea a Lavender)
     public Lavender(float x, float y) {
         super(x,y,16f,32f); // Posicion (x,y) y tamaño por PPM
@@ -16,6 +17,11 @@ public class Lavender extends Player{
     // Configuramos movimiento
     @Override
     protected void configMov() {
+        // Si esta esperando no mueve
+        if (esperaRescate) {
+            velocidad.x = 0;
+            return;
+        }
         velocidad.x = 0; // Inicializamos la velocidad como tal en el eje X
         // Cuando se mueva a la izquierda o derecha
         if (Gdx.input.isKeyPressed(Constants.LAVE_RIGHT)){
@@ -24,23 +30,37 @@ public class Lavender extends Player{
         if (Gdx.input.isKeyPressed(Constants.LAVE_LEFT)){
             velocidad.x += -moviEnX;
         }
-        // Cuando salta aplicamos gravedad
-        if (Gdx.input.isKeyPressed(Constants.LAVE_UP) && onSuelo){ // Si se presiona salta y el sprite esta en el suelo
-            velocidad.y = fuerzaSalto; // Se hace el salto y se aplica gravedad
-            onSuelo = false;
+        // Si se presiona salto, guardar en buffer
+        if (Gdx.input.isKeyPressed(Constants.LAVE_UP)) {
+            tiempoBufferSalto = MAX_BUFFER_SALTO;
         }
+        // Saltar si hay buffer activo y está en suelo
+        if (tiempoBufferSalto > 0 && onSuelo) {
+            velocidad.y = fuerzaSalto * 1.05f;
+            onSuelo = false;
+            tiempoBufferSalto = 0f;
+        }
+    }
+    public void morirEspera (float posX, float posY) {
+        // Bloquea a lavender en el sitio y activa esperaRescate
+        posicion.set(posX, posY);
+        velocidad.set(0,0);
+        esperaRescate = true;
+        vivo = true; // No muere hasta culminar contrareloj
+    }
+    public void recibirVida (float spawX, float spawY) {
+        vidas++;
+        esperaRescate = false;
+        vivo = true;
+        posicion.set(spawX, spawY);
+        velocidad.set(0,0);
+        setOnSuelo(false); // Para que mantega sobre el suelo
     }
 
     @Override
     public void update(float delta) {
         super.update(delta); // Aplica el sistema de salto y movimiento
         /* ACA VAN LAS COLISIONES, INTERACCIONES Y DEMAS */
-        // Suelo de prueba basico
-        if (posicion.y <= 0){
-            posicion.y = 0;
-            velocidad.y = 0;
-            onSuelo = true;
-        }
     }
     // Llama al batch de la clase principal
     @Override
@@ -50,4 +70,7 @@ public class Lavender extends Player{
     public Rectangle getBounds() {
         return super.getBounds();
     }
+
+    // GETTERS Y SETTERS
+    public boolean isEsperaRescate() { return esperaRescate; }
 }
