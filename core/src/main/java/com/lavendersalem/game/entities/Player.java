fa3a0e080ac45e8b61be4f6da5128e3d2a721b7d
LetmarS <letmarsevillano05@gmail.com>
2026-03-25 22:09:37 -0400

    Ajuste para cargar spriteSheet

--- core/src/main/java/com/lavendersalem/game/entities/Player.java
+++ core/src/main/java/com/lavendersalem/game/entities/Player.java
@@ -1,6 +1,9 @@
 package com.lavendersalem.game.entities;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.lavendersalem.game.utils.Constants;
 
 public abstract class Player extends Entity { // Player hereda de Entity porque es para entidades jugables
@@ -12,11 +15,29 @@ public abstract class Player extends Entity { // Player hereda de Entity porque 
     // Constantes físicas de cada persona (Definidas en Constants)
     protected float moviEnX; // Velocidad en carrera
     protected float fuerzaSalto; // Para el salto de cada personaje (contraresta gravedad)
+    protected boolean miraDer = true;
     // Para optimizar el salto en mov
     protected float tiempoBufferSalto = 0f;
     protected static final float MAX_BUFFER_SALTO = 0.1f; // 100ms de ventana
-    // Para dibujado (sprites)
+    // Para sprites
+    protected Texture sheetIdle;
+    protected Texture sheetCaminarIzq;
+    protected Texture sheetCaminarDer;
+    protected Texture sheetSaltoDer;
+    protected Texture sheetSaltoIzq;
+    protected Texture sheetInteractDer;
+    protected Texture sheetInteractIzq;
+    // Para cada frame de animaciones
+    protected Animation<TextureRegion> animIdle;
+    protected Animation<TextureRegion> animCaminarDer;
+    protected Animation<TextureRegion> animCaminarIzq;
+    protected Animation<TextureRegion> animSaltarDer;
+    protected Animation<TextureRegion> animSaltarIzq;
+    protected Animation<TextureRegion> animInteractuarDer;
+    protected Animation<TextureRegion> animInteractuarIzq;
+    protected String estadoAnim = "";
     protected float timeAnimacion = 0f;
+    protected float tiempoEnAire = 0f;
     // Constructor
     public Player(float x, float y, float tamWidth, float tamHeight, int vidasInicial) {
         super(x, y, tamWidth, tamHeight);
@@ -28,14 +49,15 @@ public abstract class Player extends Entity { // Player hereda de Entity porque 
     // Para actualizar el estado y movimiento del player
     @Override
     public void update(float delta) {
-        timeAnimacion += delta; // Acumula tiempo en cada frame
+        timeAnimacion += delta;
+        tiempoEnAire = onSuelo ? 0f : tiempoEnAire + delta;
         if (tiempoBufferSalto > 0) tiempoBufferSalto -= delta; // Reducir buffer cada frame
         configMov();
         aplicGravedad(delta);
         actualizarAnimacion();
     }
-    protected abstract void configMov(); // Cada player tiene su configuracion de Teclas
-    protected abstract void actualizarAnimacion(); // Cada palyer maneja sus frames
+    protected abstract void configMov(); // Cada player tiene su configuracion de movimientos
+    protected abstract void actualizarAnimacion(); // Cada player actualiza su frame
     // Para que el player quede sobre el suelo y en caso de salto aplica gravedad
     private void aplicGravedad(float delta) {
         if(!onSuelo){
@@ -73,6 +95,9 @@ public abstract class Player extends Entity { // Player hereda de Entity porque 
         vidas = vidasInicial;
         vivo = true;
         onSuelo = false;
+        tiempoBufferSalto = 0f;
+        estadoAnim = "";
+        timeAnimacion = 0f;
     }
 
     // GETTERS Y SETTER PARA EL GAMESCREEN
@@ -84,4 +109,7 @@ public abstract class Player extends Entity { // Player hereda de Entity porque 
 
     public int getVidas() { return vidas; }
     public void setVidas(int vidas) { this.vidas = vidas; }
+
+    // Elimina basura de la grafica
+    public abstract void dispose();
 }
