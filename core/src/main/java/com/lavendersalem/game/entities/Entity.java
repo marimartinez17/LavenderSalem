81384675840c9b63da9f2b1df495e0a2cccea4d1
LetmarS <letmarsevillano05@gmail.com>
2026-03-25 22:08:28 -0400

    Ajuste para cargar spriteSheet

--- core/src/main/java/com/lavendersalem/game/entities/Entity.java
+++ core/src/main/java/com/lavendersalem/game/entities/Entity.java
@@ -1,20 +1,18 @@
 package com.lavendersalem.game.entities;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class Entity {
-    // Todas las entidades posee una posicion y velocidad (x,y)
+    // Todas las entidades posee una posicion y velocidad (x, y)
     protected Vector2 posicion;
     protected Vector2 velocidad;
-    // Tamaño para las coliciones
+    // Tamaño para las colisiones
     protected float tamWidth;
     protected float tamHeight;
-    // Todos poseen un visual (sprites)
-    protected Texture spriteSheet;
+    // Todos poseen un visual (frame actual)
     protected TextureRegion currentFrame;
     // Constructor
     public Entity(float x, float y, float tamWidth, float tamHeight) {
@@ -25,7 +23,7 @@ public abstract class Entity {
     // Todas las entidades deben actualizarse (mov) y dibujarse
     public abstract void update(float delta);
     public abstract void draw(SpriteBatch batch);
-    // Hitbox para colisiones (Rectangulo)
+    // Hitbox para colisiones (Rectángulo)
     public Rectangle getBounds() {
         return new Rectangle(posicion.x, posicion.y, tamWidth, tamHeight);
     }
